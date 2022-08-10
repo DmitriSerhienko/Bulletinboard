397fc883d55885e2223b9d1e6aa20e9e0714415b
DmitriSerhienko <100857833+DmitriSerhienko@users.noreply.github.com>
2022-08-10 23:50:31 +0300

    updates

--- app/src/main/java/com/DimasKach/bulletinboard/dialogs/DialogSpinnerHelper.kt
+++ app/src/main/java/com/DimasKach/bulletinboard/dialogs/DialogSpinnerHelper.kt
@@ -3,13 +3,20 @@ package com.DimasKach.bulletinboard.dialogs
 import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.DimasKach.bulletinboard.R
 
 class DialogSpinnerHelper {
     fun showSpinnerDialog(context: Context, list: ArrayList<String>){
         val builder = AlertDialog.Builder(context)
         val rootView = LayoutInflater.from(context).inflate(R.layout.spinner_layout, null)
+        val adapter = RcViewDialogSpinner()
+        val rcView = rootView.findViewById<RecyclerView>(R.id.rcSpView)
+        rcView.layoutManager =LinearLayoutManager(context)
+        rcView.adapter = adapter
         builder.setView(rootView)
+        adapter.updateAdapter(list)
         builder.show()
 
     }
