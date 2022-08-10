b202c5ef447800b84237428a0ad39b868580544b
DmitriSerhienko <100857833+DmitriSerhienko@users.noreply.github.com>
2022-08-10 23:01:17 +0300

    updates

--- app/src/main/java/com/DimasKach/bulletinboard/activity/EditAdsAct.kt
+++ app/src/main/java/com/DimasKach/bulletinboard/activity/EditAdsAct.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import com.DimasKach.bulletinboard.R
 import com.DimasKach.bulletinboard.databinding.ActivityEditAdsBinding
+import com.DimasKach.bulletinboard.dialogs.DialogSpinnerHelper
 import com.DimasKach.bulletinboard.utils.CityHelper
 
 class EditAdsAct : AppCompatActivity() {
@@ -14,10 +15,10 @@ class EditAdsAct : AppCompatActivity() {
         binding = ActivityEditAdsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val adapter = ArrayAdapter(this,
-            android.R.layout.simple_spinner_item,CityHelper.getAllCountries(this))
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spCountry.adapter = adapter
+        val listCountry = CityHelper.getAllCountries(this)
+
+        val dialog = DialogSpinnerHelper()
+        dialog.showSpinnerDialog(this, listCountry)
 
     }
 
