ef45ba0a270f2137efd53b46afa9e7684ab754db
DmitriSerhienko <100857833+DmitriSerhienko@users.noreply.github.com>
2022-08-11 00:34:51 +0300

    updates

--- app/src/main/java/com/DimasKach/bulletinboard/utils/CityHelper.kt
+++ app/src/main/java/com/DimasKach/bulletinboard/utils/CityHelper.kt
@@ -4,6 +4,8 @@ import java.io.IOException
 import java.io.InputStream
 import android.content.Context
 import org.json.JSONObject
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 object CityHelper {
@@ -30,4 +32,19 @@ object CityHelper {
         }
         return tempArray
     }
+    fun filterListData(list: ArrayList<String>, searchText: String?): ArrayList<String>{
+        val tempList = ArrayList<String>()
+        tempList.clear()
+        if(searchText == null) {
+            tempList.add("No Result")
+            return tempList
+        }
+        for(selection: String in list){
+            if(selection.lowercase(Locale.ROOT).startsWith(searchText.lowercase(Locale.ROOT)))
+                tempList.add(selection)
+        }
+        if(tempList.size == 0) tempList.add("No result")
+        return tempList
+    }
+
 }
