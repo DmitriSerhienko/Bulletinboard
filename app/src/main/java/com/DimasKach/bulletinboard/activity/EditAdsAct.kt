47fcf57a67b081b375b321a8678b128dfc8ed938
DmitriSerhienko <100857833+DmitriSerhienko@users.noreply.github.com>
2022-08-11 22:15:54 +0300

    updates

--- app/src/main/java/com/DimasKach/bulletinboard/activity/EditAdsAct.kt
+++ app/src/main/java/com/DimasKach/bulletinboard/activity/EditAdsAct.kt
@@ -2,6 +2,7 @@ package com.DimasKach.bulletinboard.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import com.DimasKach.bulletinboard.R
 import com.DimasKach.bulletinboard.databinding.ActivityEditAdsBinding
@@ -9,17 +10,24 @@ import com.DimasKach.bulletinboard.dialogs.DialogSpinnerHelper
 import com.DimasKach.bulletinboard.utils.CityHelper
 
 class EditAdsAct : AppCompatActivity() {
-    private lateinit var binding: ActivityEditAdsBinding
+    lateinit var binding: ActivityEditAdsBinding
+    private val dialog = DialogSpinnerHelper()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditAdsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        init()
+    }
 
+    private fun init(){
+
+    }
+
+    //OnClicks
+
+    fun onClickSelectCountry(view: View){
         val listCountry = CityHelper.getAllCountries(this)
-
-        val dialog = DialogSpinnerHelper()
         dialog.showSpinnerDialog(this, listCountry)
-
     }
 
 }
--- app/src/main/java/com/DimasKach/bulletinboard/dialogs/RcViewDialogSpinnerAdapter.kt
+++ app/src/main/java/com/DimasKach/bulletinboard/dialogs/RcViewDialogSpinnerAdapter.kt
@@ -1,20 +1,23 @@
 package com.DimasKach.bulletinboard.dialogs
 
+import android.app.AlertDialog
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.DimasKach.bulletinboard.R
+import com.DimasKach.bulletinboard.activity.EditAdsAct
 
-class RcViewDialogSpinner: RecyclerView.Adapter<RcViewDialogSpinner.SpViewHolder>() {
-    val mainList = ArrayList<String>()
+class RcViewDialogSpinnerAdapter(var context: Context, var dialog : AlertDialog ): RecyclerView.Adapter<RcViewDialogSpinnerAdapter.SpViewHolder>() {
+    private val mainList = ArrayList<String>()
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SpViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.sp_list_item, parent, false)
 
-        return SpViewHolder(view)
+        return SpViewHolder(view, context, dialog)
     }
 
     override fun onBindViewHolder(holder: SpViewHolder, position: Int) {
@@ -25,11 +28,18 @@ class RcViewDialogSpinner: RecyclerView.Adapter<RcViewDialogSpinner.SpViewHolder
         return mainList.size
     }
 
-    class SpViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+    class SpViewHolder(itemView: View, var context: Context, var dialog: AlertDialog) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        private var itemText = ""
         fun setData(text: String){
             val tvSpItem = itemView.findViewById<TextView>(R.id.tvSpItem)
             tvSpItem.text = text
+            itemText = text
+            itemView.setOnClickListener (this)
+        }
+
+        override fun onClick(p0: View?) {
+            (context as EditAdsAct).binding.tvCountry.text = itemText
+            dialog.dismiss()
         }
     }
 
