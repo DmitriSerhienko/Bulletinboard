dfdace378248b52424f8b7af3264a321ecc63c67
DmitriSerhienko <100857833+DmitriSerhienko@users.noreply.github.com>
2022-08-28 23:42:06 +0300

    updates

--- app/src/main/java/com/DimasKach/bulletinboard/utils/ImagePicker.kt
+++ app/src/main/java/com/DimasKach/bulletinboard/utils/ImagePicker.kt
@@ -1,8 +1,16 @@
 package com.DimasKach.bulletinboard.utils
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.DimasKach.bulletinboard.activity.EditAdsAct
 import com.fxn.pix.Options
 import com.fxn.pix.Pix
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 object ImagePicker {
     const val REQUEST_CODE_IMAGES = 999
@@ -17,5 +25,32 @@ object ImagePicker {
             .setScreenOrientation(Options.SCREEN_ORIENTATION_PORTRAIT)
             .setPath("/pix/images")
         Pix.start(context , options)
+    }
+
+    fun showSelectedImages(requestCode: Int, resultCode: Int, data: Intent?, edAct: EditAdsAct){
+        if (resultCode == AppCompatActivity.RESULT_OK && requestCode == REQUEST_CODE_IMAGES) {
+            if (data != null) {
+                val returnValues = data.getStringArrayListExtra(Pix.IMAGE_RESULTS)
+                if (returnValues?.size!! > 1 && edAct.chooseImageFragment == null) {
+                    edAct.openChooseImageFrag(returnValues)
+                } else if (returnValues.size == 1 && edAct.chooseImageFragment == null) {
+                    //edAct.imageAdapter.update(returnValues)
+                    //val tempList = ImageManager.getImageSize(returnValues[0])
+                    CoroutineScope(Dispatchers.Main).launch {
+                        edAct.binding.pBarLoad.visibility = View.VISIBLE
+                        val bitMapArray = ImageManager.imageResize(returnValues) as ArrayList<Bitmap>
+                        edAct.binding.pBarLoad.visibility = View.GONE
+                        edAct.imageAdapter.update(bitMapArray)
+                    }
+                } else if (edAct.chooseImageFragment != null) {
+                    edAct.chooseImageFragment?.upDateAdapter(returnValues)
+                }
+            }
+        } else if (resultCode == AppCompatActivity.RESULT_OK && requestCode == REQUEST_CODE_SINGLE_IMAGES) {
+            if (data != null) {
+                val uris = data.getStringArrayListExtra(Pix.IMAGE_RESULTS)
+                edAct.chooseImageFragment?.setSingleImage(uris?.get(0)!!, edAct.editImagePos)
+            }
+        }
     }
 }
