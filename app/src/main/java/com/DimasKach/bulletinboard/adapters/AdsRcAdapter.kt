bf7d58a3c8387b3edb60363e197627f0b0481490
DmitriSerhienko <100857833+DmitriSerhienko@users.noreply.github.com>
2022-09-01 22:35:48 +0300

    updates

---
+++ app/src/main/java/com/DimasKach/bulletinboard/adapters/AdsRcAdapter.kt
@@ -0,0 +1,41 @@
+package com.DimasKach.bulletinboard.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.DimasKach.bulletinboard.data.Ad
+import com.DimasKach.bulletinboard.databinding.AdListItemBinding
+
+class AdsRcAdapter: RecyclerView.Adapter<AdsRcAdapter.AdHolder>() {
+    val adArray = ArrayList<Ad>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdHolder {
+        val binding = AdListItemBinding.inflate(LayoutInflater.from(parent.context))
+        return AdHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: AdHolder, position: Int) {
+        holder.setData(adArray[position])
+    }
+
+    override fun getItemCount(): Int {
+        return adArray.size
+    }
+
+    fun upDateAdapter(newList: List<Ad>){
+        adArray.clear()
+        adArray.addAll(newList)
+        notifyDataSetChanged()
+
+    }
+
+    class AdHolder(val binding: AdListItemBinding): RecyclerView.ViewHolder(binding.root) {
+        fun setData(ad: Ad){
+            binding.apply{
+                tvDescription.text = ad.description
+                tvPrice.text = ad.price
+
+            }
+        }
+    }
+}
